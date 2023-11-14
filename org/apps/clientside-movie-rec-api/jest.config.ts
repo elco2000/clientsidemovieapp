5fed0716d260919bb24d064a61cb4659f049fa03
Elco-PC <elcomussert@gmail.com>
2023-11-14 14:45:10 +0100

    Add api applications

---
+++ org/apps/clientside-movie-rec-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'clientside-movie-rec-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/clientside-movie-rec-api',
+};
