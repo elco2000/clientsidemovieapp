eb2bd5315c2f0daa81c9a25f3b5cbbd0c6d743d8
Elco-PC <elcomussert@gmail.com>
2023-11-19 22:08:07 +0100

    Added about page

--- org/libs/clientside-movie-web/features/src/lib/about/about.component.ts
+++ org/libs/clientside-movie-web/features/src/lib/about/about.component.ts
@@ -1,11 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
 
 @Component({
     selector: 'org-about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
     constructor() {}
+
+    imageErd?: string;
+    imageEntiteit?: string;
+    imageScenarios?: string;
+    imageQuerys?: string;
+
+    ngOnInit(): void {
+        this.imageErd = "assets/ERD.png";
+        this.imageEntiteit = "assets/Entiteiten.png"
+        this.imageScenarios = "assets/Scenarios.png"
+        this.imageQuerys = "assets/Querys.png"
+    }
 }
