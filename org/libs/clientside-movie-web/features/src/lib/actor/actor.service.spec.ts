5da66ca05bb38e5a44ec116219aee72c8c41ac99
Elco-PC <elcomussert@gmail.com>
2023-11-26 15:58:40 +0100

    Added start testing

---
+++ org/libs/clientside-movie-web/features/src/lib/actor/actor.service.spec.ts
@@ -0,0 +1,14 @@
+import { ActorService } from "./actor.service";
+import { TestBed } from "@angular/core/testing";
+
+const testActor = {
+    _id: 1,
+    name: 'Elco Mussert',
+    birthdate: new Date(),
+    nationality: 'Dutch',
+    photo: 'test.url'
+}
+
+describe('ActorService', () => {
+    
+})
