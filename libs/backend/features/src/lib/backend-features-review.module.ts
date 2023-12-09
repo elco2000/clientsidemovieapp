2d659c6122e0fb8067669ac86f1f86932efd0354
Elco-PC <elcomussert@gmail.com>
2023-12-09 14:18:32 +0100

    Added auth to calls

--- libs/backend/features/src/lib/backend-features-review.module.ts
+++ libs/backend/features/src/lib/backend-features-review.module.ts
@@ -2,11 +2,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { ReviewController } from './review/review.controller';
 import { ReviewService } from './review/review.service';
+import { AuthModule } from '@org/backend/auth';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
     controllers: [ReviewController],
     providers: [ReviewService],
-    imports: [MongooseModule.forRoot('mongodb+srv://elcoAdmin:Admin123Database@clusterlocaldataapi.m0cwagc.mongodb.net/')],
+    imports: [MongooseModule.forRoot('mongodb+srv://elcoAdmin:Admin123Database@clusterlocaldataapi.m0cwagc.mongodb.net/'),
+                AuthModule,
+                JwtModule],
     exports: [ReviewService]
 })
 export class BackendFeaturesReviewModule {}
